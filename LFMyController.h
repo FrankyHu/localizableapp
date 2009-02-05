0c7f27000f2ac951d9c329a16128084e53cff34b
Hu Chin-Hao <davidhu@hu-chin-haos-macbook-pro-15.local>
2009-02-05 21:48:02 +0800

    Adds source code files
    Updates .gitignore

---
+++ LFMyController.h
@@ -0,0 +1,21 @@
+//
+//  LFMyController.h
+//  firstApp
+//
+//  Created by Hu Chin-Hao on 2/4/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface LFMyController : NSWindowController {
+	IBOutlet NSTableView *_view;
+	NSMutableArray *_LFLSTRlist;
+	NSMutableArray *_LFLSTR2list;
+	NSMutableArray *_Displaylist;
+	
+
+}
+-(void) parse:(NSString *)filePath;
+@end
---
+++ LFMyController.m
@@ -0,0 +1,185 @@
+//
+//  LFMyController.m
+//  firstApp
+//
+//  Created by Hu Chin-Hao on 2/4/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "LFMyController.h"
+
+
+@implementation LFMyController
+
+- (void) dealloc
+{
+	[_LFLSTRlist release];
+	[_LFLSTR2list release];
+	[_Displaylist release];
+	[super dealloc];
+}
+
+-(void) parse:(NSString *)filePath
+{
+	NSMutableArray * array = [NSMutableArray new];
+	//Pass 1: Scan for macro LFLSTR in project directory
+	NSString *file;
+	NSString *path;
+	NSString *aStr;
+	const NSString *s;
+	_LFLSTRlist = [NSMutableArray new];
+	_LFLSTR2list = [NSMutableArray new];
+	_Displaylist = [NSMutableArray new];
+	NSDirectoryEnumerator *dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:filePath];
+	while (file = [dirEnum nextObject]) {
+		if ([[file pathExtension] isEqualToString: @"m"]) {
+			path = [filePath stringByAppendingPathComponent:file];
+			aStr = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+			NSString *searchForLFLSTR = @"LFLSTR";
+			NSString *searchForLFLSTR2 = @"LFLSTR2";
+			NSRange rangeForLFLSTR = [aStr rangeOfString : searchForLFLSTR];
+			NSRange rangeForLFLSTR2 = [aStr rangeOfString : searchForLFLSTR2];
+			if ( (rangeForLFLSTR.location != NSNotFound) || (rangeForLFLSTR2.location != NSNotFound) ) {
+				[array addObject:path];
+			}
+		}
+	}
+
+	//Pass 2
+	for(id x in array){
+		s = [NSString stringWithContentsOfFile:x encoding:NSUTF8StringEncoding error:nil];
+		int i, j;
+		//Looking for LFLSTR
+		for (i = 0 ;i < [s length] ;i++ ) {
+			if ([s characterAtIndex:i] == 'L') {
+				if((i + 6) < [s length] && [[s substringWithRange:NSMakeRange(i, 9)] isEqualToString:@"LFLSTR(@\""]) {
+					NSMutableString *param = [NSMutableString string];
+					i += 9;
+					BOOL end = NO;
+					for (j = 0; !end; j++){
+						if (i + j >= [s length]) {
+							break;
+						}
+						if ([s characterAtIndex:(i+j)] == '"' && [s characterAtIndex:(i+j-1)] != '\\' ) {
+							end = YES;
+							continue;
+						}
+						[param appendFormat:@"%C",[s characterAtIndex:(i+j)]];
+					}
+					i += j;
+					[_LFLSTRlist addObject:param];
+				}
+			}
+		}
+		//Looking for LFLSTR2
+		for (i = 0 ;i < [s length] ;i++ ) {
+			if ([s characterAtIndex:i] == 'L'){
+				if((i + 7) < [s length] && [[s substringWithRange:NSMakeRange(i, 10)] isEqualToString:@"LFLSTR2(@\""]) {
+					NSMutableArray *LFLSTR2 = [NSMutableArray new];
+					NSMutableString *param1 = [NSMutableString string];
+					NSMutableString *param2 = [NSMutableString string];
+					i += 10;
+					BOOL end = NO;
+					for (j = 0; !end; j++){
+						if (i + j >= [s length]) {
+							break;
+						}
+						if ([s characterAtIndex:(i+j)] == '"' && [s characterAtIndex:(i+j-1)] != '\\' ) {
+							end = YES;
+							continue;
+						}
+						[param1 appendFormat:@"%C",[s characterAtIndex:(i+j)]];
+					}
+					[LFLSTR2 addObject:param1];
+					i = i + j + 3;
+					end = NO;
+					//',' '@' '"'
+					for (j = 0; !end; j++) {
+						if (i + j >= [s length]) {
+							break;
+						}
+						if ([s characterAtIndex:(i+j)] == '"' && [s characterAtIndex:(i+j-1)] != '\\' ) {
+							end = YES;
+							continue;
+						}
+						[param2 appendFormat:@"%C",[s characterAtIndex:(i+j)]];
+					}
+					i = i + j;
+					[LFLSTR2 addObject:param2];
+					[_LFLSTR2list addObject:LFLSTR2];
+				}
+			}
+		}
+	}
+	//Merge two arrays to one
+	for (id x in _LFLSTRlist) {
+		NSMutableArray *COL = [NSMutableArray new];
+		[COL addObject:x];
+		[COL addObject:x];
+		[COL addObject:@""];
+		[_Displaylist addObject:COL];
+	}
+	for (id key in _LFLSTR2list) {
+		NSMutableArray *COL = [NSMutableArray new];
+		for (id value in key) {
+			
+			[COL addObject:value];
+			[COL addObject:value];
+		}
+		[_Displaylist addObject:COL];
+	}
+	
+	[_view reloadData];
+}
+
+- (void)awakeFromNib
+{
+	[[self window] center];
+	[[self window] setDelegate:self];
+	//Read LFLSTR & LFLSTR2 to arrays
+	[self parse:[NSHomeDirectory() stringByAppendingPathComponent:  @"Documents/RamdomApp/"]];
+}
+
+#pragma mark NSTableDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+	return ([_Displaylist count]);
+}
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+	NSString *columnName = [aTableColumn identifier];
+	if ( [columnName isEqualToString:@"col1"] ) {
+		return [[_Displaylist objectAtIndex:rowIndex] objectAtIndex:0];
+	}
+	else if ( [columnName isEqualToString:@"col2"] ) {
+		return [[_Displaylist objectAtIndex:rowIndex] objectAtIndex:1];
+	}
+	else if ( [columnName isEqualToString:@"col3"] ) {
+		return [[_Displaylist objectAtIndex:rowIndex] objectAtIndex:2];
+	}
+	return nil;
+}
+
+
+- (void)tableView:(NSTableView *)aTableView setObjectValue:(id)anObject forTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+	NSString *columnName = [aTableColumn identifier];
+	if ( [columnName isEqualToString:@"col1"] ) {
+		[[_Displaylist objectAtIndex:rowIndex] replaceObjectAtIndex:0 withObject:anObject];
+	}else if ( [columnName isEqualToString:@"col2"] ) {
+		[[_Displaylist objectAtIndex:rowIndex] replaceObjectAtIndex:1 withObject:anObject];
+	}else if ( [columnName isEqualToString:@"col3"] ) {
+		[[_Displaylist objectAtIndex:rowIndex] replaceObjectAtIndex:2 withObject:anObject];
+	}
+}
+
+#pragma mark NSWindow
+
+- (void)windowWillClose:(NSNotification *)notification
+{
+	[[NSApplication sharedApplication] terminate:nil];
+}
+
+@end
